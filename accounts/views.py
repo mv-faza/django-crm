af3245b33a765952617ad969146f32a29ea7a94b
Faza <fazli-musaev@mail.ru>
2021-06-13 20:56:43 +0500

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from .models import *
+# Create your views here.
+
+def home(request):
+    orders = Order.objects.all()
+    customer = Customer.objects.all()
+    context = {'orders': orders, 'customers':customers}
+    return render(request,'accounts/dashboard.html')
+
+def products(request):
+    products=Product.objects.all()
+    return render(request,'accounts/products.html', { 'products': products})
+
+
+
+def customer(request):
+    return render(request,'accounts/customer.html')
