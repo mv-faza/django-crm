ad9e9b1e72210aab3571a8771edba093d44ad541
Faza <fazli-musaev@mail.ru>
2021-06-13 21:22:33 +0500

    removed view.py

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,5 @@ def products(request):
     products=Product.objects.all()
     return render(request,'accounts/products.html', { 'products': products})
 
-
-
 def customer(request):
     return render(request,'accounts/customer.html')
