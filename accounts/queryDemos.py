ba193f29f067acdd55763417c5ec6d4c3dd57eba
Faza <fazli-musaev@mail.ru>
2021-06-15 16:03:51 +0500

    make query set

---
+++ accounts/queryDemos.py
@@ -0,0 +1,33 @@
+customers = Customer.objects.all()
+firstCustomer = Customer.objects.first()
+lastCustomer = Customer.objects.last()
+customerByName = Customer.objects.get(name='Peter Piper')
+customerById = Customer.objects.get(id=4)
+firstCustomer.order_set.all()
+order = Order.objects.first() 
+parentName = order.customer.name
+products = Product.objects.filter(category="Out Door")
+leastToGreatest = Product.objects.all().order_by('id') 
+greatestToLeast = Product.objects.all().order_by('-id') 
+productsFiltered = Product.objects.filter(tags__name="Sports")
+
+ballOrders = firstCustomer.order_set.filter(product__name="Ball").count()
+
+allOrders = {}
+
+for order in firstCustomer.order_set.all():
+	if order.product.name in allOrders:
+		allOrders[order.product.name] += 1
+	else:
+		allOrders[order.product.name] = 1
+
+
+class ParentModel(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+class ChildModel(models.Model):
+	parent = models.ForeignKey(Customer)
+	name = models.CharField(max_length=200, null=True)
+
+parent = ParentModel.objects.first()
+parent.childmodel_set.all()
