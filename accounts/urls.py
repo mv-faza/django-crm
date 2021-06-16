6cf90348f9e855846c8ae2910a68425409b55325
Faza <fazli-musaev@mail.ru>
2021-06-16 11:56:10 +0500

    make dynamic url routing and temlates

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('products/',views.products),
-    path('customer/',views.customer),
+    path('customer/<str:pk_test>/', views.customer),
 ]
