13f1ee12d6077f2a23b9255217554c8b1dc1391a
fazliddin musaev <fazli-musaev@mail.ru>
2021-06-22 08:07:41 +0500

    table search filter

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
 from django.forms import inlineformset_factory
+from .filters import OrderFilter
 # Create your views here.
 
 def home(request):
@@ -29,7 +30,9 @@ def customer(request, pk_test):
 	customer = Customer.objects.get(id=pk_test)
 	orders = customer.order_set.all()
 	order_count = orders.count()
-	context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+	myFilter = OrderFilter(request.GET, queryset=orders)
+	orders = myFilter.qs 
+	context = {'customer':customer, 'orders':orders, 'order_count':order_count,'myFilter':myFilter}
 	return render(request, 'accounts/customer.html',context)
 
 def createOrder(request, pk):
