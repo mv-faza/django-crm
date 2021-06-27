391e9e26e21b9a557844815337028ebc65235cbd
fazliddin musaev <fazli-musaev@mail.ru>
2021-06-27 11:03:24 +0500

    add image file upload and change user profile,make migration and creat html for accaount settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
 	path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
 
     path('', views.home, name="home"),
-    path('user/',views.userPage, name="user-page"),
+    path('user/', views.userPage, name="user-page"),
+
+    path('account/', views.accountSettings, name="account"),
+
     path('products/', views.products, name='products'),
     path('customer/<str:pk_test>/', views.customer, name="customer"),
 
--- crm1/settings.py
+++ crm1/settings.py
@@ -131,3 +131,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
