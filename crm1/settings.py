a231072b9293d89000c2c1f2a866bb2f77d82873
fazliddin musaev <fazli-musaev@mail.ru>
2021-07-01 18:33:00 +0500

    add password reset mail

--- crm1/settings.py
+++ crm1/settings.py
@@ -132,3 +132,13 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'fazlimusaev1@gmail.com'
+EMAIL_HOST_PASSWORD = 'your mail password'
